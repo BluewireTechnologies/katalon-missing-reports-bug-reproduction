89e5ab42e1037f700e07eaa8d80c473e0daaa950
Andy Davies <andydavies92@outlook.com>
2018-03-07 14:47:55 +0000

    Clone the test suite so there are 5 in total

---
+++ ReproductionProject/Test Suites/ReproductionTestSuite4.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ReproductionTestSuite4</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>117b1530-479b-468f-b405-3e907447f6c4</testSuiteGuid>
+   <testCaseLink>
+      <guid>c1b594e5-c5d6-4fac-8a7a-f98b77730dac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReproductionTestCase1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d70504b9-9f6f-492b-b6f6-b268560038a1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReproductionTestCase2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2c5b1d6d-35f6-43d8-acbd-912cb6140457</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReproductionTestCase3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c252d436-45b2-4394-8b81-c7f9a8306b91</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReproductionTestCase4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>36495cea-9c1a-4d7c-bd7c-6fc893672639</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReproductionTestCase5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
